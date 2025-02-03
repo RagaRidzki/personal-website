88316a9df0a502b14ee0c52f8531e1f6ed97eab2
RagaRidzki <ragaridzkyy@gmail.com>
2025-02-03 21:15:25 +0700

    add(vercel.json):fix 404 error on another page

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
-import Project from "./pages/Project.jsx"
+import Project from "./pages/Project.jsx";
 import Certificate from "./pages/Certificate.jsx";
 import Contact from "./pages/Contact.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/about",
-    element: <About />
-  },
-  {
-    path: "/project",
-    element: <Project />
-  },
-  {
-    path: "/certificate",
-    element: <Certificate />
-  },
-  {
-    path: "/contact",
-    element: <Contact />
-  }
-]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/certificate" element={<Certificate />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
